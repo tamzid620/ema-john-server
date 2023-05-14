ab68f572dc8eee09462de09ee1654992edcacea5
tamzid620 <tamzid620@gmail.com>
2023-05-14 23:05:23 +0600

    products added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 // ------------------------------------
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.qtemx5j.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtemx5j.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,14 +24,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
+    const productCollection = client.db("emaJohnDB").collection("products");
+
+    app.get('/products' , async(req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    })
+
     await client.connect();
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    // await client.close();
   }
 }
 run().catch(console.dir);
